7d0dcb1f9723eabc54251194804b5e14516581cc
Henry Noyes <noyes.he@northeastern.edu>
2026-03-27 12:41:44 -0400

    tone down blink completion

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = { auto_show = false },
+        ghost_text = { enabled = false },
+      },
+    },
+  },
+}
